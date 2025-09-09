1d81367292e0ce7ad1acfec5b607c47e47ab059b
Eurekoko <niuzhihong321@gmail.com>
2025-09-09 17:27:42 -0400

    refactored to extract helper method

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -1,4 +1,18 @@
 package lab1;
 
 public class Reduce {
+    public static int reduce(int n) {
+        int total = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+                total++;
+            } else if (n % 2 != 0) {
+                n -= 1;
+                total++;
+            }
+        }
+        return total;
+    }
+
 }
