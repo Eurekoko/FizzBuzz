fbcbe531d632fac4e8bd1f092b70d7c2e32b146d
Eurekoko <niuzhihong321@gmail.com>
2025-09-09 00:33:56 -0400

    refactored to extract helper method

--- test/lab1/Multiples.java
+++ test/lab1/Multiples.java
@@ -1,4 +1,19 @@
 package lab1;
 
-public class Multiples {
+class Multiples{
+    public static int multiples(int n, int a, int b) {
+        int i = 1;
+        int count = 0;
+        while(i < n) {
+            if(i % a == 0 || i % b == 0) {
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
 }
